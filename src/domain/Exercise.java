7e8888e24361220dae2824a9085ef685b21c5e1e
zourabchik <44872022+zourabchik@users.noreply.github.com>
2020-04-24 20:18:03 +0300

    Updated.

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,23 @@
 package domain;
-
+/** Нахождение второго по величине числа в заданном массиве
+* @author Katia Zourab
+*/
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+	public static int SecondNumber(int[] numbersArray) {
+		int biggest= numbersArray[0];
+        int secondBiggest = numbersArray[0];
+        for (int i = 0; i < numbersArray.length; i++)
+        {
+        	if (numbersArray[i] > biggest)
+        	{
+        		secondBiggest = biggest;
+        		biggest = numbersArray[i];
+        	}
+        	else if (numbersArray[i] > secondBiggest && numbersArray[i] != biggest)
+        	{
+        		secondBiggest = numbersArray[i];
+        		}
+        	}
+        return secondBiggest;
+	}
 }
